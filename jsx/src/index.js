b4778ccd55e7a17844747a22d3f2787c7822ee0e
replacem3nts <donny.landis@gmail.com>
2020-10-03 11:30:25 -0400

    completes JSX walkthrough

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi There!</div>;
+  const buttonText = { text: 'Click me' };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
