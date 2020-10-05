aa9a08b1ff9602ec75cc9e6ffa1c67dec5401c23
replacem3nts <donny.landis@gmail.com>
2020-10-05 12:20:33 -0400

    completes initial pics setup

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit = (term) => {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID -b8FuaeG2Dw8w9sXXNcdYbEwDA-aC3yFbqyllXEdsnQ',
+      },
+    });
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
