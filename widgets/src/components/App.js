34aa6ab9ffed7438f91a475b880b81a09a848caa
replacem3nts <donny.landis@gmail.com>
2020-10-07 13:06:06 -0400

    completes setup of widgets proj

---
+++ widgets/src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library amongst engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
