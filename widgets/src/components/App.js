afdcbb5fa12e961ac6ff6498f0634879b186c39b
replacem3nts <donny.landis@gmail.com>
2020-10-08 11:47:48 -0400

    finishes search widget

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Search from './Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library amongst engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library amongst engineers',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components',
+//   },
+// ];
 
 export default () => {
   return (
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,16 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log('using effect!');
-  }, [term]);
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [results.length, term]);
 
   // Second argument to useEffect:
   //  [] (empty array) - runs at initial render
   //  nothing (zero arg) - runs at initial render + every rerender
   //  [data] (some number of elements in array) - run at initial render and after every rerender IF data has changed since last render
+
+  const renderedResults = results.map(result => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            GO
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -24,6 +69,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
