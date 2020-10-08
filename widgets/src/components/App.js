3d1325b11cfb4cf45f55053c1560bbbb7fe9fa77
replacem3nts <donny.landis@gmail.com>
2020-10-08 17:48:08 -0400

    completes scaffolding for translate widget

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Dropdown from './Dropdown';
+// import Dropdown from './Dropdown';
+import Translate from './Translate';
 
 // const items = [
 //   {
@@ -16,37 +17,25 @@ import Dropdown from './Dropdown';
 //   },
 // ];
 
-const options = [
-  {
-    label: 'The Color Red',
-    value: 'red',
-  },
-  {
-    label: 'The Color Green',
-    value: 'green',
-  },
-  {
-    label: 'A Shade of Blue',
-    value: 'blue',
-  },
-];
+// const options = [
+//   {
+//     label: 'The Color Red',
+//     value: 'red',
+//   },
+//   {
+//     label: 'The Color Green',
+//     value: 'green',
+//   },
+//   {
+//     label: 'A Shade of Blue',
+//     value: 'blue',
+//   },
+// ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
