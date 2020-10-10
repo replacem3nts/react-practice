ca00e5f677f70364a2828298a67f7590d1667702
replacem3nts <donny.landis@gmail.com>
2020-10-10 14:46:27 -0400

    completes simple redux proj songs

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,4 +1,6 @@
-export const songsReducer = () => {
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
     { title: 'Rape Me', duration: '3:36' },
@@ -7,10 +9,15 @@ export const songsReducer = () => {
   ];
 };
 
-export const selectedSongReducer = (selectedSong = null, action) => {
+const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
