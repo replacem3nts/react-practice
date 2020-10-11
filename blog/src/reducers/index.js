f8d8679bef4357fd3bfb5f97d7580c8b5049b023
replacem3nts <donny.landis@gmail.com>
2020-10-11 14:03:30 -0400

    completes blog application

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
-  replaceMe: () => 10,
+  posts: postsReducer,
+  users: userReducer,
 });
