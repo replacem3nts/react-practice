80bc9ff83cac0901a6f13a36e479a49dd2c31823
replacem3nts <donny.landis@gmail.com>
2020-10-12 19:41:11 -0400

    begins implementing oauth

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '1087610763901-vptvdghq1192ugo4h7ote6j9p0as0rfb.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in!</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
