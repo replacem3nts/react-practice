da1bb016dfc819e65aee8b72818404cd1bf1abeb
replacem3nts <donny.landis@gmail.com>
2020-10-17 18:16:01 -0400

    completes CRU actions for stream

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -28,7 +28,6 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
-
   history.push('/');
 };
 
@@ -45,9 +44,10 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
