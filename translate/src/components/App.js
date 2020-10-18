bb2da7948eb6db735b2a50ad0b4875b9bdde7a36
replacem3nts <donny.landis@gmail.com>
2020-10-18 12:49:29 -0400

    scaffolds the translate app

---
+++ translate/src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag es"
+            onClick={() => this.onLanguageChange('spanish')}
+          />
+          <i
+            className="flag de"
+            onClick={() => this.onLanguageChange('german')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ translate/src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends Component {
+  static contextType = LanguageContext;
+
+  render() {
+    return <button className="ui button primary">Submit</button>;
+  }
+}
+
+export default Button;
